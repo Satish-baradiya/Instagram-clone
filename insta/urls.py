b4a919776bda474b04fc7ce3651048fd39b96679
Satish-baradiya <baradiyasatish@gmail.com>
2022-11-17 11:49:59 +0530

    User signup and signin completed.

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .import views
-
+from django.contrib.auth import views as authviews
 urlpatterns = [
-    path('',views.test,name='test')
+    path('',views.home,name='home'),
+    path('register',views.register,name='register'),
+    path('login',authviews.LoginView.as_view(template_name='insta/login.html'),name='login'),
+    path('logout',authviews.LogoutView.as_view(template_name='insta/logout.html'),name='logout')
 ]
--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegistrationForm
 # Create your views here.
 
-def test(request):
-    return HttpResponse("Welcome to instagram")
+
+
+def home(request):
+    return render(request,'insta/home.html')
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, f'Your account has been created. You can log in now!')    
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'insta/register.html', context)
